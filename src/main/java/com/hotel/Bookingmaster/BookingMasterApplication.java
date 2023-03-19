eae02465ee7d9167763ea9f59ed8deb23affe44e
vijay-jatti <vijay.jatti@hpe.com>
2023-03-19 20:00:51 +0530

    initial commit

---
+++ src/main/java/com/hotel/Bookingmaster/BookingMasterApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.Bookingmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMasterApplication.class, args);
+	}
+
+}
