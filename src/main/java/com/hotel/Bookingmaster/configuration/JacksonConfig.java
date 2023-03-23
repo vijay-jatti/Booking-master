ec00979947e062a7763d647e70bc506377bb310a
vijay-jatti <vijay.jatti@hpe.com>
2023-03-24 00:33:29 +0530

    chore: moved Jackson configuration to configuration package

--- src/main/java/com/hotel/Bookingmaster/configuration/JacksonConfig.java
+++ src/main/java/com/hotel/Bookingmaster/configuration/JacksonConfig.java
@@ -1,4 +1,4 @@
-package com.hotel.Bookingmaster;
+package com.hotel.Bookingmaster.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
